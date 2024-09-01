405c0db62dd110d3b15a6486b9238c47f397b7e7
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-01 11:36:53 -0300

    feat: Adicionado o metodo de buscar por cnpj

--- src/main/java/com/julio/tgid/repository/CompanyRepository.java
+++ src/main/java/com/julio/tgid/repository/CompanyRepository.java
@@ -3,7 +3,9 @@ package com.julio.tgid.repository;
 import com.julio.tgid.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    Optional<Company> findByCnpj(String cnpj);
 }
