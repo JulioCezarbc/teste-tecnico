0de630b91b15c089af6458ea6264bc8a86b4162c
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-01 02:09:56 -0300

    feat: Metodo para buscar por cpf

--- src/main/java/com/julio/tgid/repository/ClientRepository.java
+++ src/main/java/com/julio/tgid/repository/ClientRepository.java
@@ -1,8 +1,11 @@
 package com.julio.tgid.repository;
 
+import com.julio.tgid.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ClientRepository extends JpaRepository<ClientRepository, UUID> {
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+    Optional<Client> findByCpf(String cpf);
 }
