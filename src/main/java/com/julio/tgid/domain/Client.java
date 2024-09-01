941dbb502ff86f5fba0dc7eef34d40072598e07d
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-09-01 00:54:49 -0300

    Feat: Domain Client

--- src/main/java/com/julio/tgid/domain/Client.java
+++ src/main/java/com/julio/tgid/domain/Client.java
@@ -2,8 +2,10 @@ package com.julio.tgid.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -18,10 +20,83 @@ public class Client {
     private String firstName;
     @Column(length = 20, nullable = false)
     private String lastName;
+    @Column(unique = true, nullable = false, length = 11)
+    private String cpf;
     @Column(unique = true, nullable = false)
     @Email
     private String email;
+    @Column(nullable = false)
+    @Positive
     private BigDecimal saldo;
 
+    public Client(){}
+    public Client(UUID id, String firstName, String lastName, String cpf, String email, BigDecimal saldo) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cpf = cpf;
+        this.email = email;
+        this.saldo = saldo;
+    }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email String email) {
+        this.email = email;
+    }
+
+    public @Positive BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(@Positive BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
